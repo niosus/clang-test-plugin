77f0e7833d87c698da5b70c1dec4713e1b009f75
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2017-03-14 23:02:01 +0100

    add more info on what it is and how to run

--- script.py
+++ script.py
@@ -4,15 +4,32 @@ import sys
 import platform
 from os import path
 sys.path.append(path.dirname(__file__))
-from clang import cindex40
+from clang import cindex38
+from clang import cindex40_old
+from clang import cindex40_new
 from clang import utils
 
-cindex = cindex40
+# cindex = cindex38      # works
+# cindex = cindex40_new  # works
+cindex = cindex40_old  # fails
+
 ClangUtils = utils.ClangUtils
 filename = path.abspath(path.join(path.dirname(__file__), "test.cpp"))
-clang_dir = ClangUtils.find_libclang_dir("clang++")
+
+if len(sys.argv) > 1:
+  clang_binary = sys.argv[1]
+else:
+  clang_binary = 'clang++'
+
+clang_dir = ClangUtils.find_libclang_dir(clang_binary)
 if not cindex.Config.loaded:
     cindex.Config.set_library_path(clang_dir)
 print("Clang directory = ", clang_dir)
+print("libclang lib = ", path.join(cindex.Config.library_path,
+                                   ClangUtils.libclang_name))
 print("Platform: ", platform.architecture())
 print("Python version: ", platform.python_version())
+clang_flags = ['-x', 'c++', '-std=c++11']
+index = cindex.Index.create()
+index.parse(path=filename, args=clang_flags)
+del index
--- test.cpp
+++ test.cpp
@@ -1,15 +1,3 @@
-#include <string>
-#include <vector>
-
-struct Foo {
-  int bar[];
-};
-
 int main(int argc, char const* argv[]) {
-  std::vector<std::string> vec;
-  vec.push_back("hello");
-  for (const auto& s : vec) {
-    s.c_str();
-  }
-  Foo f;
+  // no content needed
 }
