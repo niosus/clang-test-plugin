499dc2807ad3936087457aa4eaffcc3093cadaf0
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2017-03-14 21:36:32 +0100

    add cindex and remove old ones

--- script.py
+++ script.py
@@ -4,11 +4,10 @@ import sys
 import platform
 from os import path
 sys.path.append(path.dirname(__file__))
-from clang import cindex38
+from clang import cindex40
 from clang import utils
 
-cindex = cindex38
-TU = cindex.TranslationUnit
+cindex = cindex40
 ClangUtils = utils.ClangUtils
 filename = path.abspath(path.join(path.dirname(__file__), "test.cpp"))
 clang_dir = ClangUtils.find_libclang_dir("clang++")
@@ -17,20 +16,3 @@ if not cindex.Config.loaded:
 print("Clang directory = ", clang_dir)
 print("Platform: ", platform.architecture())
 print("Python version: ", platform.python_version())
-clang_flags = ['-x', 'c++', '-std=c++11']
-tu = TU.from_source(filename=filename, args=clang_flags)
-if tu is not None:
-    print("YAY! Parsed TranslationUnit")
-else:
-    print("something went wrong")
-tu.reparse()
-compl_obj = tu.codeComplete(path=filename, line=4, column=7)
-print(len(compl_obj.results))
-
-tu.reparse()
-compl_obj = tu.codeComplete(path=filename, line=4, column=7)
-print(len(compl_obj.results))
-
-for diag in tu.diagnostics:
-	print("here")
-	print(diag)
--- test.cpp
+++ test.cpp
@@ -1,5 +1,15 @@
+#include <string>
 #include <vector>
-int main(int argc, char const *argv[]) {
-  std::vector<int> vec;
-  vec.
+
+struct Foo {
+  int bar[];
+};
+
+int main(int argc, char const* argv[]) {
+  std::vector<std::string> vec;
+  vec.push_back("hello");
+  for (const auto& s : vec) {
+    s.c_str();
+  }
+  Foo f;
 }
